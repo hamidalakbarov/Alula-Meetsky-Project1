4caf93035c1b9afc67f2c24e942272a234877557
saja Al sheikh <S.alsheikh9@gmail.com>
2023-03-08 13:06:59 -0600

    US08_SajaAlsheikh Done

--- src/test/java/net/meetsky/step_definitions/Meetsky_StepDefinitions.java
+++ src/test/java/net/meetsky/step_definitions/Meetsky_StepDefinitions.java
@@ -3,6 +3,7 @@ package net.meetsky.step_definitions;
 import io.cucumber.java.en.*;
 import net.meetsky.pages.BasePage;
 import net.meetsky.pages.DashboardPage;
+import net.meetsky.pages.FilePage;
 import net.meetsky.pages.LoginPage;
 import net.meetsky.utilities.BrowserUtils;
 import net.meetsky.utilities.ConfigurationReader;
@@ -10,7 +11,10 @@ import net.meetsky.utilities.Driver;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,4 +68,50 @@ public class Meetsky_StepDefinitions {
         Assert.assertEquals(expectedModules,actualModules);
 
     }
+
+    //Saja US
+    FilePage filePage = new FilePage();
+    Actions actions = new Actions(Driver.getDriver());
+    @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
+        Driver.getDriver().get("https://qa.meetsky.net");
+        loginPage.userNameInputBox.sendKeys("User1");
+        loginPage.passwordInputBox.sendKeys("Userpass123");
+        loginPage.loginButton.click();
+
+    }
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module(String File) {
+      filePage.File.click();
+    }
+    @When("user clicks the add icon on the top")
+    public void user_clicks_the_add_icon_on_the_top() {
+        actions.moveToElement(filePage.AddIcon);
+        filePage.AddIcon.click();
+        BrowserUtils.sleep(3);
+
+    }
+    @When("user click \"new folder”")
+    public void user_click_new_folder() {
+        filePage.NewFolder.click();
+
+    }
+    @When("user write a folder name")
+    public void user_write_a_folder_name() {
+        filePage.inputNewFolder.sendKeys("Saja Folder");
+        BrowserUtils.sleep(2);
+    }
+    @When("the user click submit icon")
+    public void the_user_click_submit_icon() {
+        filePage.submit.click();
+        BrowserUtils.sleep(2);
+    }
+    @Then("Verify the folder is displayed on the page")
+    public void verify_the_folder_is_displayed_on_the_page() {
+        filePage.SelectFiles.isDisplayed();
+        String expected = "Saja Folder";
+
+       Assert.assertTrue(expected,filePage.SelectFiles.isDisplayed());
+    }
+
 }
