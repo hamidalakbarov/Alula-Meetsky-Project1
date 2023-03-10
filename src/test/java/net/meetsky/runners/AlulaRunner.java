707b07e37f01c9669b8c4b6ceaa9031c2d921acd
hamidalakbarov <hamidalakbarov@gmail.com>
2023-03-10 09:57:01 -0800

    Final one

--- src/test/java/net/meetsky/runners/AlulaRunner.java
+++ src/test/java/net/meetsky/runners/AlulaRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json"
         },
         publish = true,
-        tags = "@hamid",
+        tags = "@regression",
         dryRun = false
 
 )
